a7a159398ef39a8a71b0a718c4b4e51286450d16
erikrios <erikriosetiawan49@gmail.com>
2019-08-25 10:09:20 +0700

    Basic functions

---
+++ src/main/kotlin/BasicFunctions.kt
@@ -0,0 +1,7 @@
+fun sayMyName(name: String): String {
+    return "Your name is $name"
+}
+
+fun saysMyName(name: String): String = "Your name is $name"
+
+fun sayMyNames(name: String) = "Your name is $name"
